0131db5adb09041d1c9dca49a079d609d4f11625
LegenDrags <77605763+dragsbruh@users.noreply.github.com>
2023-11-25 18:48:59 +0530

    Revert dumb mistake

--- index.js
+++ index.js
@@ -9,28 +9,6 @@ const app = express();
 const wss = new WebSocketServer({ port: 8000 });
 
 app.use(express.json());
-app.use(function (req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "*");
-	const allowedOrigins = [
-		"http://localhost:3000",
-		"http://gamebrag.onrender.com",
-		"https://gamebrag.onrender.com",
-	];
-	const origin = req.headers.origin;
-	if (allowedOrigins.includes(origin)) {
-		res.setHeader("Access-Control-Allow-Origin", origin);
-	}
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-	);
-	res.header("Access-Control-Allow-credentials", true);
-	res.header(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, UPDATE"
-	);
-	next();
-});
 
 var topics = new Map();
 var subscribers = new Map();
